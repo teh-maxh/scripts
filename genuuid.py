9944ba3b5d58ff1cfb99e9084565b1f3fff3f60e
Max Harmony <maxh@maxh.name>
2021-11-15 05:44:47 -0500

    reflow logic

--- genuuid.py
+++ genuuid.py
@@ -5,18 +5,18 @@ import sys
 
 argc = len(sys.argv)
 
-if(argc == 1 or argc > 3):
-	print("Usage: genuuid [domain] name");
-	quit();
-
 if(argc == 2):
 	from socket import gethostname
 	domain = gethostname().split('.', 1)[1]
 	name = sys.argv[1];
 
-if(argc == 3):
+elif(argc == 3):
 	domain = sys.argv[1];
 	name = sys.argv[2];
+
+else:
+	print("Usage: genuuid [domain] name");
+	quit();
 
 uuid = str(uuid.uuid5(uuid.uuid5(uuid.NAMESPACE_DNS,domain),name));
 print(uuid);
