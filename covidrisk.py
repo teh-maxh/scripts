3b88ebb76be8de21ca77f642d9b4721cdd606510
Max Harmony <maxh@maxh.name>
2022-05-21 00:19:34 -0400

    extreme covid risk support

--- covidrisk.py
+++ covidrisk.py
@@ -7,13 +7,13 @@ import os, sys
 fp = open(os.path.join(sys.path[0],"covidrisk.conf"), 'r')
 conf = json.load(fp)
 
-risk = ["\033[32mLow\033[0m", "\033[33mModerate\033[0m", "\033[38;2;192;106;11mSubstantial\033[0m", "\033[31mHigh\033[0m", "Unknown"]
+risk = ["\033[32mLow\033[0m", "\033[33mModerate\033[0m", "\033[38;2;192;106;11mSubstantial\033[0m", "\033[31mHigh\033[0m", "Unknown", "\033[1;31mExtreme\033[0m"]
 
 stateData = json.loads(urllib.request.urlopen(f"https://api.covidactnow.org/v2/state/{conf['state']}.json?apiKey={conf['apiKey']}").read())
 countyData = json.loads(urllib.request.urlopen(f"https://api.covidactnow.org/v2/county/{conf['fips']}.json?apiKey={conf['apiKey']}").read())
 
 overall = risk[max(stateData['riskLevels']['caseDensity'],stateData['riskLevels']['infectionRate'],stateData['riskLevels']['testPositivityRatio'],countyData['riskLevels']['caseDensity'],countyData['riskLevels']['infectionRate'],stateData['riskLevels']['testPositivityRatio'])]
-risk[0] += "\t"; risk[3] += "\t"
+risk[0] += "\t"; risk[3] += "\t"; risk[4] += "\t"
 resultsTable = f"\tNew cases\tInfection rate\tPos. tests\nState\t{risk[stateData['riskLevels']['caseDensity']]}\t{risk[stateData['riskLevels']['infectionRate']]}\t{risk[stateData['riskLevels']['testPositivityRatio']]}\nCounty\t{risk[countyData['riskLevels']['caseDensity']]}\t{risk[countyData['riskLevels']['infectionRate']]}\t{risk[countyData['riskLevels']['testPositivityRatio']]}"
 
 print(f"You are at {overall.lower()} risk.\nDetails\n{resultsTable}")
