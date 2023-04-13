3f9488253bd673c1d42fc930a38e2196ea79540a
Max Harmony <maxh@maxh.name>
2023-04-13 03:14:37 -0400

    covid risk calculation when some risk factors are unknown

--- covidrisk.py
+++ covidrisk.py
@@ -12,8 +12,22 @@ risk = ["\033[32mLow\033[0m", "\033[33mModerate\033[0m", "\033[38;2;192;106;11mS
 stateData = json.loads(urllib.request.urlopen(f"https://api.covidactnow.org/v2/state/{conf['state']}.json?apiKey={conf['apiKey']}").read())
 countyData = json.loads(urllib.request.urlopen(f"https://api.covidactnow.org/v2/county/{conf['fips']}.json?apiKey={conf['apiKey']}").read())
 
-overall = risk[max(stateData['riskLevels']['caseDensity'],stateData['riskLevels']['infectionRate'],stateData['riskLevels']['testPositivityRatio'],countyData['riskLevels']['caseDensity'],countyData['riskLevels']['infectionRate'],stateData['riskLevels']['testPositivityRatio'])]
+factors = [stateData['riskLevels']['caseDensity'],stateData['riskLevels']['infectionRate'],stateData['riskLevels']['testPositivityRatio'],countyData['riskLevels']['caseDensity'],countyData['riskLevels']['infectionRate'],stateData['riskLevels']['testPositivityRatio']]
+i = 0
+for n in factors:
+	if (n == 4):
+		allKnown = False
+		factors[i] = -1
+		i+=1
+	
+
+overall = risk[max(factors)]
 risk[0] += "\t"; risk[3] += "\t"; risk[4] += "\t"; risk[5] += "\t";
 resultsTable = f"\tNew cases\tInfection rate\tPos. tests\nState\t{risk[stateData['riskLevels']['caseDensity']]}\t{risk[stateData['riskLevels']['infectionRate']]}\t{risk[stateData['riskLevels']['testPositivityRatio']]}\nCounty\t{risk[countyData['riskLevels']['caseDensity']]}\t{risk[countyData['riskLevels']['infectionRate']]}\t{risk[countyData['riskLevels']['testPositivityRatio']]}"
 
-print(f"You are at {overall.lower()} risk.\nDetails\n{resultsTable}")
+if (allKnown):
+	print(f"You are at {overall.lower()} risk.")
+else:
+	print("Not all risk factors are known.")
+	print(f"Based on known risk factors, you are at {overall.lower()} risk.")
+print(f"Details\n{resultsTable}")
